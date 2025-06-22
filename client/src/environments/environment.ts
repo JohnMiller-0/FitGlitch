2150101fbda4e3aadad0122d1ac7c0f2789777c2
John Miller <john.miller27@snhu.edu>
2025-06-22 10:59:38 -0400

    FitGlitch Application

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 // client/src/environments/environment.ts  (dev)
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://fitglitch.onrender.com/api'
 };
