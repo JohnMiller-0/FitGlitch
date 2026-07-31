6250ce32eb1bc995e4babaa1ee6beff66044e5d4
John Miller <john.miller27@snhu.edu>
2026-07-31 17:49:48 -0400

    Update API URL for production environment

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // client/src/environments/environment.prod.ts  (prod)
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://fitglitch.onrender.com/api'
 };
